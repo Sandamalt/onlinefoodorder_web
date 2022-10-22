65b962f1834baaf49e5d398f0779f9021afb6939
Sandamal Tharuka <sandamal.live@gmail.com>
2022-10-22 18:00:47 +0530

    Day one scond commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,39 @@
+import styles from "../styles/Navbar.module.css"
+import Image from "next/image"
+
 const Navbar = () => {
     return (
-        <div>
-                Navbar
+        <div className={styles.container}>
+            <div className={styles.item}>
+            <div className={styles.callButton}> 
+            <img src="/img/telephone.png" alt="" width="32" height="32" /> 
+                </div>        
+                  <div className={styles.texts}> 
+                  <div className={styles.text}> ORDER NOW ! </div>
+                    <div className={styles.text}>036 20 30 500 </div>
+                  </div>    
+                     </div>
+
+            <div className={styles.item}> 
+                <ul className={styles.list}> 
+                    <li className={styles.listItem}> Homepage </li>
+                    <li className={styles.listItem}> Product </li>
+                    <li className={styles.listItem}> Menu </li>
+                    <img src="/img/logo.png" alt="" width="160px" height="69px" />
+                    <li className={styles.listItem}> Events </li>
+                    <li className={styles.listItem}> Blog </li>
+                    <li className={styles.listItem}> Contact </li>
+                </ul>
+             </div>
+            <div className={styles.item}> 
+            <div className={styles.cart}> 
+            <div> <img src="/img/cart.png" alt="" width="30px" height="30px" /> </div>
+            <div className={styles.counter}> 2 </div>
+            </div>
+              
         </div>
-    )
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
